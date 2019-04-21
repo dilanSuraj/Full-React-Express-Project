19978e7bee412d424d35e639ddd6bdab426982a5
dilanSuraj <40181843+dilanSuraj@users.noreply.github.com>
2019-04-21 20:14:55 +0530

    AF Project Test Phase, Front End Development

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+import TodoList from "./components/todos-list.component";
+import TodoEdit from "./components/edit-todo.component";
+import TodoCreate from "./components/create-todo.component";
+
+import logo from "./logo.svg";
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+                            <img src={logo} width="30" height="30" alt="Cosing the smartway image here"/>
+                        </a>
+                        <Link to="/" className="navbar-brand">Home ToDo App</Link>
+                        <div className="collapse navbar-collapse">
+                           <ul className="navbar-nav mr-auto">
+                               <li className="navbar-item">
+                                   <Link to="/" className="nav-link">ToDos</Link>
+                               </li>
+                               <li className="navbar-item">
+                                   <Link to="/create" className="nav-link">Create ToDos</Link>
+                               </li>
+                           </ul>
+                        </div>
+                    </nav>
+                    <Route path="/" exact component={TodoList}/>
+                    <Route path="/edit/:id" component={TodoEdit}/>
+                    <Route path="/create" component={TodoCreate}/>
+                </div>
+
+            </Router>
+        );
+    }
 }
 
 export default App;
